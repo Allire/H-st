babcbb4172f04792d4e27e9f5c17503348b889c5
Oskar Bergman <oskar.bergman86@gmail.com>
2017-05-29 23:14:44 +0200

    Example JavaDoc

--- Stable/src/stable/Horse.java
+++ Stable/src/stable/Horse.java
@@ -32,7 +32,19 @@ public class Horse{
       this.rank = rankNR; 
       
   }
-  
+    /**
+     * Returns an Image object that can then be painted on the screen.
+     * The url argument must specify an absolute. The name
+     * argument is a specifier that is relative to the url argument.
+     * <p>
+     * This method always returns immediately, whether or not the
+     * image exists. When this applet attempts to draw the image on
+     * the screen, the data will be loaded. The graphics primitives
+     * that draw the image will incrementally paint on the screen.
+     *
+     * @param  weight  an absolute URL giving the base location of the image
+     * @return      the image at the specified URL
+     */
   public void setWeight(double weight){
       this.weight = weight;
   }
